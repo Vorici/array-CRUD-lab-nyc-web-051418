95369fbd0509b4cc10dde498c0d9e82bb805fd6f
Ledio Bode <l.bode515@gmail.com>
2018-05-12 20:34:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  planets.pop
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  planets.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
