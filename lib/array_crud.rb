d150da8700f08facada109c9f3c8d646099b722c
Ledio Bode <l.bode515@gmail.com>
2018-05-12 18:30:00 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  jovial = [""]
+  jovial = ["jupiter","saturn","uranus","neptune"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  jovial << "earth"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  jovial.unshift 
 end
 
 def remove_element_from_end_of_array(array)
